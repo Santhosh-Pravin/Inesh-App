d61a8f3dc40a02a4082e30bdc08efa853ad8a4dc
Madlab41 <santypravin3@gmail.com>
2026-06-24 11:17:46 +0530

    New Main modified

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    )
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Inesh Smart Energy',
+      //theme: AppTheme.dark,
+      //home: const LoginScreen(),
+    );
+  }
+}
